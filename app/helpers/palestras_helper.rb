4d8d3ebb198b821994d5d872cf81852d8cab70c0
Andre Rodrigues <andrenpaes@gmail.com>
2011-07-01 15:29:57 -0300

    Palestras em Andamento

--- app/helpers/palestras_helper.rb
+++ app/helpers/palestras_helper.rb
@@ -2,6 +2,13 @@ module PalestrasHelper
   include ActionView::Helpers::NumberHelper
 
   def em_andamento?(palestra)
-    palestra.data == Date.today and palestra.horario.hour == Time.now.hour
+    agora = Time.now
+    hora_palestra = palestra.horario.hour + (palestra.horario.min / 60.0)
+    puts hora_palestra
+    if palestra.data == Date.today
+      puts hora_palestra, hora_palestra.ceil, palestra.horario
+      return true if hora_palestra.ceil == agora.hour
+      return true if palestra.horario.hour == agora.hour and palestra.horario.min >= agora.min
+    end
   end
 end
