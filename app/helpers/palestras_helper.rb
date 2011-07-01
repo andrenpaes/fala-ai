956bbbb4c6efda6c9ebc3050992ca1baad790e38
Andre Rodrigues <andrenpaes@gmail.com>
2011-07-01 15:09:18 -0300

    Ajustes

--- app/helpers/palestras_helper.rb
+++ app/helpers/palestras_helper.rb
@@ -1,3 +1,7 @@
 module PalestrasHelper
   include ActionView::Helpers::NumberHelper
+
+  def em_andamento?(palestra)
+    palestra.data == Date.today and palestra.horario.hour == Time.now.hour
+  end
 end
