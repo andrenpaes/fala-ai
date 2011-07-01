e7eb493ae19f488d783bdbf95d3d71b12b8c95f2
Andre Rodrigues <andrenpaes@gmail.com>
2011-07-01 19:46:48 -0300

    Correcao

--- app/helpers/palestras_helper.rb
+++ app/helpers/palestras_helper.rb
@@ -4,9 +4,7 @@ module PalestrasHelper
   def em_andamento?(palestra)
     agora = Time.now
     hora_palestra = palestra.horario.hour + (palestra.horario.min / 60.0)
-    puts hora_palestra
     if palestra.data == Date.today
-      puts hora_palestra, hora_palestra.ceil, palestra.horario
       return true if hora_palestra.ceil == agora.hour
       return true if palestra.horario.hour == agora.hour and palestra.horario.min >= agora.min
     end
